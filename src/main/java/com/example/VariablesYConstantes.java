5a9790c2e0d10e55a3b2be580dc1730abb2fee87
Brandon Gonzalez <Brandon.osorio.gonzalez98@gmail.com>
2026-05-16 11:28:38 -0500

    Ejercicio-1

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,6 +3,9 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad= 27;
+        double estatura= 1.72;
+        System.out.println("su edad es de:" + edad + "su estatura es : "+ estatura);
     }
 
     public static void demostrarConvencionesNombres() {
